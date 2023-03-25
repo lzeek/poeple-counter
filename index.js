df0e893bef72c12f689682b7ae6be10571245ae8
lzeek <86994603+lzeek@users.noreply.github.com>
2023-03-25 11:10:35 +0200

    Error display
    Added feature to display error if save button was clicked and no increment was made

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let peopleNum= document.getElementById('people-cnt')
 let incrementBtn= document.getElementById('increment-btn')
 let saveBtn = document.getElementById('save-btn')
 let lastCountNum = document.getElementById('last-count')
+let errorDisplay =document.getElementById('error-display')
 
-
-let peopleTotal = 0
-
+let peopleTotal = 0 //Number of people per increament before saved
+let peopleGroups=[]
 
 
 function incrementCount(){
@@ -18,10 +18,17 @@ function resetCounter(){
     peopleTotal=0
     peopleNum.textContent=peopleTotal
     lastCountNum.textContent=''
+    errorDisplay.textContent=''
 }
 
 function saveCount(){
-    lastCountNum.textContent+=`${peopleTotal} | `
-    peopleNum.textContent = 0
-    peopleTotal = 0
+    if(peopleTotal>0){
+        lastCountNum.textContent+=`${peopleTotal} | ` //updates per groups
+        peopleNum.textContent = 0
+        errorDisplay.textContent=''
+        peopleGroups.push(peopleTotal)
+        peopleTotal = 0
+    }else{
+        errorDisplay.textContent='No increment made there is nothing to save. Pointless saving a Zero'
+    }
 }

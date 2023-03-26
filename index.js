9a0144f5fe0d6f484e1a3dbd7775dc33a95943a7
lzeek <86994603+lzeek@users.noreply.github.com>
2023-03-26 14:57:57 +0200

    Display overall counts
    Added overallcounts increments since counts started

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ let incrementBtn= document.getElementById('increment-btn')
 let saveBtn = document.getElementById('save-btn')
 let lastCountNum = document.getElementById('last-count')
 let errorDisplay =document.getElementById('error-display')
+let peopleCounted=document.getElementById('people-counted')
 
 let peopleTotal = 0 //Number of people per increament before saved
-let peopleGroups=[]
-
+let peopleGroups=0 //total number of all increaments ever made since reset
 
 function incrementCount(){
     peopleTotal+=1
+    peopleGroups+=1
     peopleNum.textContent=peopleTotal
     
 }
 
 function resetCounter(){
     peopleTotal=0
+    peopleGroups=0
     peopleNum.textContent=peopleTotal
     lastCountNum.textContent=''
     errorDisplay.textContent=''
+    peopleCounted.textContent=0
 }
 
 function saveCount(){
@@ -26,9 +29,9 @@ function saveCount(){
         lastCountNum.textContent+=`${peopleTotal} | ` //updates per groups
         peopleNum.textContent = 0
         errorDisplay.textContent=''
-        peopleGroups.push(peopleTotal)
         peopleTotal = 0
+        peopleCounted.textContent=peopleGroups
     }else{
-        errorDisplay.textContent='No increment made there is nothing to save. Pointless saving a Zero'
+        errorDisplay.textContent='No increment made, there is nothing to save. Pointless saving a Zero!'
     }
 }
